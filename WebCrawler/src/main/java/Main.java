fe3f2ebaace755ca40b4b6997cb976663c42d40c
Matej Bujdak <matej.bujdak2@gmail.com>
2023-07-12 14:03:49 +0200

    fix: uprava WebCrawler.iml

--- WebCrawler/src/main/java/Main.java
+++ WebCrawler/src/main/java/Main.java
@@ -16,7 +16,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         try {
             InputReader inputReader = new InputReader();
             Crawler crawler = new Crawler();
